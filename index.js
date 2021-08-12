c2cb991317364b7d52c76251b6d74cb237f0e82e
Iceecode <iceehiphop1@gmail.com>
2021-08-12 16:18:42 -0700

    NodeJS Installs
    - Socket.io
    - Express

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-var jikanjs  = require('jikanjs'); // Uses per default the API version 3
+// Database
 
+
+// Server
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/main.html');
+});
+app.use(express.static('client'));
+
+serv.listen(2000);
+console.log('server started');
+
+// Sockets
+var io = require('socket.io')(serv, {});
+
+// JikanJS
+var jikanjs  = require('jikanjs'); // Uses per default the API version 3
+// Simple Rate Limiter - Using on JikanJS to rate limit
 var limit = require("simple-rate-limiter");
 var request = limit(require("jikanjs")).to(2).per(1000);
 
-var i = 1;
-
 var username = 'Iceehiphop';
 
+/*
+var i = 1;
 jikanjs.loadUser(username, 'animelist', 'watching').then((response) => {
     response.anime.forEach(element => {
         console.log(i + ` ${element.title}`);
@@ -15,21 +35,8 @@ jikanjs.loadUser(username, 'animelist', 'watching').then((response) => {
 }).catch((err) => {
     console.error(err); // in case a error happens
 });
+*/
 
-jikanjs.loadUser(username, 'animelist', 'completed').then((response) => {
-    response.anime.forEach(element => {
-        console.log(i + ` ${element.title}`);
-        i++;
-    });
-}).catch((err) => {
-    console.error(err); // in case a error happens
-});
-
-jikanjs.loadUser(username, 'animelist', 'onhold').then((response) => {
-    response.anime.forEach(element => {
-        console.log(i + ` ${element.title}`);
-        i++;
-    });
-}).catch((err) => {
-    console.error(err); // in case a error happens
+io.sockets.on('connection', function(socket) {
+    console.log('socket connection');
 });
